a2de2aa36ee4014293c62864df84145344e9b43a
husainvohra93 <husain.vohra10@gmail.com>
2025-05-21 10:57:44 +0530

    AfterVideo40-Redis
    AfterVideo40-Redis

--- src/main/java/com/example/JournalApplication/service/WeatherService.java
+++ src/main/java/com/example/JournalApplication/service/WeatherService.java
@@ -5,6 +5,7 @@ package com.example.JournalApplication.service;
 import com.example.JournalApplication.api.response.WeatherResponse;
 import com.example.JournalApplication.cache.AppCache;
 import com.example.JournalApplication.constants.Placeholders;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
@@ -13,6 +14,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
+@Slf4j
 public class WeatherService {
     @Value("${weather.apiKey}")
     private String apiKey;
@@ -23,15 +25,31 @@ public class WeatherService {
     @Autowired
     private AppCache appCache;
 
+    @Autowired
+    private RedisService redisService;
+
     public WeatherResponse getWeather(String city) {
 
-        /* In Below statement it is appCache.APP_CACHE.get(AppCache.keys.WEATHER_API.toString()) ---> Gets the value of
-        "WEATHER_API" key from the Map. Map was already
-         initialized by database values at time of Bean Creation by
-         function @PostConstruct init.*/
+        /*below checks if response is cached in redis , then gets the data from redis ,
+        if response is not cached in redis then makes Weather API call*/
+        WeatherResponse weatherResponse = redisService.get("weather_of_" + city, WeatherResponse.class);
+        if(weatherResponse!=null) {
+            log.info("Response from Redis");
+            return weatherResponse;
+        } else{
 
-        String finalAPI = appCache.appCache.get(AppCache.keys.WEATHER_API.toString()).replace(Placeholders.CITY,city).replace(Placeholders.API_KEY,apiKey);
-        ResponseEntity<WeatherResponse> response =  restTemplate.exchange(finalAPI, HttpMethod.GET, null, WeatherResponse.class);
-        return response.getBody();
+            /* In Below statement it is appCache.APP_CACHE.get(AppCache.keys.WEATHER_API.toString()) ---> Gets the value of
+            "WEATHER_API" key from the Map. Map was already
+            initialized by database values at time of Bean Creation by
+            function @PostConstruct init.*/
+
+            String finalAPI = appCache.appCache.get(AppCache.keys.WEATHER_API.toString()).replace(Placeholders.CITY,city).replace(Placeholders.API_KEY,apiKey);
+            ResponseEntity<WeatherResponse> response =  restTemplate.exchange(finalAPI, HttpMethod.GET, null, WeatherResponse.class);
+            WeatherResponse body = response.getBody();
+            if(body!=null){
+                redisService.set("weather_of_" + city,body, 300L);
+            }
+            return body;
+        }
     }
 }
